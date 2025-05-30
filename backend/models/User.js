9afb9465207a2c85c1527480cd24805aa0133c7a
Ashray A <127750953+Ashray-A@users.noreply.github.com>
2025-05-31 01:30:35 +0530

    day 2

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,65 +1,62 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-  {
-    githubId: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    avatarUrl: {
-      type: String,
-      required: true,
-    },
-    githubUrl: {
-      type: String,
-      required: true,
-    },
-    accessToken: {
-      type: String,
-      required: true,
-    },
-    joinedAt: {
-      type: Date,
-      default: Date.now,
-    },
-    lastActive: {
-      type: Date,
-      default: Date.now,
-    },
-    totalCommits: {
-      type: Number,
-      default: 0,
-    },
-    currentStreak: {
-      type: Number,
-      default: 0,
-    },
-    longestStreak: {
-      type: Number,
-      default: 0,
-    },
-    lastCommitDate: {
-      type: Date,
-      default: null,
-    },
+const userSchema = new mongoose.Schema({
+  githubId: {
+    type: String,
+    required: true,
+    unique: true
   },
-  {
-    timestamps: true,
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  avatarUrl: {
+    type: String,
+    required: true
+  },
+  githubUrl: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    required: true
+  },
+  joinedAt: {
+    type: Date,
+    default: Date.now
+  },
+  lastActive: {
+    type: Date,
+    default: Date.now
+  },
+  totalCommits: {
+    type: Number,
+    default: 0
+  },
+  currentStreak: {
+    type: Number,
+    default: 0
+  },
+  longestStreak: {
+    type: Number,
+    default: 0
+  },
+  lastCommitDate: {
+    type: Date,
+    default: null
   }
-);
+}, {
+  timestamps: true
+});
 
-userSchema.methods.updateActivity = function () {
+userSchema.methods.updateActivity = function() {
   this.lastActive = new Date();
   return this.save();
 };
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
